d2ce27fdf2ed05b69bca7605e4abb04e1b1ee61b
Kelby Dinkel <kelbyd1@live.com>
2017-03-23 09:58:27 -0500

    created the menu file

--- GameDesign/Assets/Scripts/station/upgradeMenu/menu.cs
+++ GameDesign/Assets/Scripts/station/upgradeMenu/menu.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class menu : MonoBehaviour {
-    int scrollPosition;
+    Vector2 scrollPosition = Vector2.zero;
     int numSel;
+    List<string> preUpgrades = new List<string>();
+    string[] upgrades;
+    public static bool activated = false;
 	// Use this for initialization
 	void Start () {
-		
+        //I need to delete this before I actually use this menu
+        upgrades = preUpgrades.ToArray();
 	}
 	
 	// Update is called once per frame
@@ -16,9 +20,9 @@ public class menu : MonoBehaviour {
 	}
     private void OnGUI()
     {
-        /*
-        scrollPosition = GUI.BeginScrollView(new Rect(Screen.width / 2 - 110, Screen.height / 2, 220, 200), scrollPosition, new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 100 * hello.Length));
-        numSel = (GUI.SelectionGrid(new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 100 * hello.Length), numSel, hello, 1));
+        if (activated)
+        scrollPosition = GUI.BeginScrollView(new Rect(Screen.width / 2 - 110, Screen.height / 2, 220, 200), scrollPosition, new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 100 * upgrades.Length));
+        numSel = (GUI.SelectionGrid(new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 100 * upgrades.Length), numSel, upgrades, 1));
         GUI.EndScrollView();
         if (GUI.Button(new Rect(Screen.width / 2 - 145, Screen.height / 2 + 220, 90, 50), "Load Scene"))
         {
@@ -26,9 +30,8 @@ public class menu : MonoBehaviour {
         }
         if (GUI.Button(new Rect(Screen.width / 2 - 45, Screen.height / 2 + 220, 90, 50), "Cancel"))
         {
-            popSaves = false;
-            con.SetActive(false);
+            
         }
-        */
+        
     }
 }
